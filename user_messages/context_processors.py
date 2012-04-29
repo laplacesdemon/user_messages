da8803f4d8e86b31d83a066eeaba76cafe1f1c8c
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2012-04-29 16:59:57 +0300

    added unread count to the context

--- user_messages/context_processors.py
+++ user_messages/context_processors.py
@@ -5,4 +5,6 @@ def user_messages(request):
     c = {}
     if request.user.is_authenticated():
         c["inbox_count"] = Thread.objects.inbox(request.user).count()
+        c["unread_count"] = Thread.objects.unread(request.user).count()
     return c
+
