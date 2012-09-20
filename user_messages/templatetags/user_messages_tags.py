0e5fb6061f291f0ddb4194d1790852aea9021fb5
Suleyman Melikoglu <suleyman@melikoglu.info>
2012-09-20 15:42:41 +0300

    Update user_messages/templatetags/user_messages_tags.py

--- user_messages/templatetags/user_messages_tags.py
+++ user_messages/templatetags/user_messages_tags.py
@@ -8,3 +8,23 @@ def unread(thread, user):
     Returs true if there is unread messages for the user on the thread
     """
     return bool(thread.userthread_set.filter(user=user, unread=True))
+
+@register.filter
+def other_user(thread, user):
+    """
+    Returns the other user in the thread
+    """
+    for u in thread.users.all():
+        if u != user:
+            return u
+    return None
+
+@register.filter
+def other_user_profile(thread, user):
+    """
+    Returns the other user in the thread
+    """
+    for u in thread.users.select_related().all():
+        if u != user:
+            return u
+    return None
